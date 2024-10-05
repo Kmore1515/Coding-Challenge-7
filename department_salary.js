e6c829b326b505ba5492e84e912780b87d155651
Kmore1515 <kylebradleymorris@usf.edu>
2024-10-05 17:16:20 -0400

    Initialize company structure with departments and employees

---
+++ department_salary.js
@@ -0,0 +1,104 @@
+// Task 1: Create a Department Structure
+ const company = {
+
+         departments: [
+    
+             {
+    
+                 departmentName: 'Marketing', // Marketing Department with employees and their subordinates.
+    
+                 employees: [
+    
+                     {
+    
+                         name: 'Kyle',
+    
+                         salary: 100000,
+    
+                         subordinates: [
+    
+                             {
+    
+                                 name: 'Nicolas',
+    
+                                 salary: 80000,
+    
+                                 subordinates: [
+    
+                                     {
+    
+                                         name: 'Brandon',
+    
+                                         salary: 60000,
+    
+                                         subordinates: []
+    
+                                     }
+    
+                                 ]
+    
+                             }
+    
+                         ]
+    
+                     },
+    
+                     {
+    
+                         name: 'Jeremy',
+    
+                         salary: 90000,
+    
+                         subordinates: []
+    
+                     }
+    
+                 ]
+    
+             },
+    
+             {
+    
+                 departmentName: 'Sales', // Sales department with employees and their subordinates.
+    
+                 employees: [
+    
+                     {
+    
+                         name: 'Jennifer',
+    
+                         salary: 85000,
+    
+                         subordinates: [
+    
+                             {
+    
+                                 name: 'Bill',
+    
+                                 salary: 70000,
+    
+                                 subordinates: []
+    
+                             }
+    
+                         ]
+    
+                     },
+    
+                     {
+    
+                         name: 'Billy',
+    
+                         salary: 95000,
+    
+                         subordinates: []
+    
+                     }
+    
+                 ]
+    
+             }
+    
+         ]
+    
+     };
